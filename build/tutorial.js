e085a07d5ceb488c502c9db5026c139d814a8769
Sean Omlor <somlor@eml.cc>
2014-02-11 00:52:47 -0800

    comment form and basic submission handler

--- build/tutorial.js
+++ build/tutorial.js
@@ -45,10 +45,23 @@ var CommentList = React.createClass({displayName: 'CommentList',
 });
 
 var CommentForm = React.createClass({displayName: 'CommentForm',
+  handleSubmit: function () {
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if (!text || !author) {
+      return false;
+    }
+    // TODO: send request to server
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+    return false;
+  },
   render: function () {
     return (
-      React.DOM.div( {className:"commentForm"}, 
-        " Hello, world! I am a CommentForm. "
+      React.DOM.form( {className:"commentForm", onSubmit:this.handleSubmit}, 
+        React.DOM.input( {type:"text", placeholder:"Your name", ref:"author"} ),
+        React.DOM.input( {type:"text", placeholder:"Say something ...", ref:"text"} ),
+        React.DOM.input( {type:"submit", value:"Post"} )
       )
     );
   }
