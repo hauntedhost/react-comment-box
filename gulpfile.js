a88956de23ebb121fad02cf40ae79cc8ead6e21b
Sean Omlor <somlor@eml.cc>
2014-02-11 20:43:54 -0800

    update gulp file to catch/notify errors
    vs. hard crashing

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var notify = require('gulp-notify');
 var concat = require('gulp-concat');
 var react = require('gulp-react');
 
 gulp.task('templates', function () {
   gulp.src('./src/*.jsx')
       .pipe(react())
+        .on('error', gutil.log)
+        .on('error', gutil.beep)
+        .on('error', notify.onError('ERROR: jsx templates error'))
       .pipe(gulp.dest('./build'))
+      .pipe(notify('jsx templates rebuilt'));
 });
 
 gulp.task('watch', function () {
